f8577e8e4ac31551bf1706c69c20ec3c37e4ccc9
Ramazan Zholdas <61927828+RamazanZholdas@users.noreply.github.com>
2023-06-15 09:24:51 +0600

    Update Signin.tsx

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -10,7 +10,7 @@ const Signin = () => {
   const submit = async (e: any) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:8000/login', {
+    const response = await fetch('https://redis.training.edetekapps.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
